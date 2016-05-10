800f46d04a7969f5ecc5821baeb2ad0390eac706
jbf <faden@cottagesystems.com>
2016-05-10 06:11:23 -0500

    whoops, I didn't mean to commit that change.

--- src/org/netbeans/modules/nbexpose/ExposeTableModel.java
+++ src/org/netbeans/modules/nbexpose/ExposeTableModel.java
@@ -90,7 +90,6 @@ public class ExposeTableModel extends AbstractTableModel {
             }
             return b;
         } else if (columnIndex == 1) {
-            nodes[rowIndex].getClientProperty(this)
             return nodes[rowIndex];
         } else if (columnIndex == 2) {
             return getNodeType(nodes[rowIndex]);
